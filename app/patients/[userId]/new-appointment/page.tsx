200d5ef4dd627818b42b5e473a6421141b9c5782
eldin-s <eldin_s12@hotmail.com>
2024-10-18 20:38:20 +0200

    Layout, auth, database and sms notification (initial commit)

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,43 @@
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+import Image from "next/image";
+
+export default async function NewAppointment({
+  params: { userId },
+}: SearchParamProps) {
+  const patient = await getPatient(userId);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      {/* OTP Verification || PassKey */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/CareLine.png"
+            alt="CareLine"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+
+          <p className="copyright mt-10 py-12">© 2024 CareLine</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        width={1000}
+        height={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
